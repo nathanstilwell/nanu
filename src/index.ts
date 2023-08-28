54dc963a6e20d0e13c04857e7b0bcfb630eeb1c7
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2023-08-28 17:10:59 -0400

    starter source

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+/* Nathan's Numerals */
+const NathanNumerals = [
+  "•", "|", "—",
+  "+", "x", "=",
+  "△", "⊓", "ϟ",
+  "⏥", "⧖", "⩕",
+] as const;
+type NathanNumeralID = typeof NathanNumerals[number];
+const nanu: Record<NathanNumeralID, number> = {
+  "⩕": 117147,
+  "⧖": 59049,
+  "⏥": 19683,
+  "ϟ": 6561,
+  "⊓": 2187,
+  "△": 729,
+  "=": 243,
+  "x": 81,
+  "+": 27,
+  "—": 9,
+  "|": 3,
+  "•": 1,
+} as const;
+
+// this function is meant to be generic
+const toNumerals = <T extends string>(
+  num: number,
+  numerals: Record<T, number>,
+  numeralAccumulator: string[] = [],
+) => {
+  if (numerals === undefined) {
+    throw new Error(
+      "Numeral set undefined. A base set of numerals must be supplied",
+    );
+  }
+  if (num < 0) {
+    return toNumerals(Math.abs(num),  numerals, ["-", ...numeralAccumulator],);
+  }
+
+  const next = Object.keys(numerals).find((n) => Math.floor(num / numerals[n]));
+
+  if (next) {
+    return toNumerals(
+      num - numerals[next],
+      numerals,
+      [...numeralAccumulator, next],
+    );
+  }
+
+  return numeralAccumulator.join("");
+};
+
+export const toNanu = (num: number) => {
+  // may need to add a way to reverse numeral systems
+  const nu = toNumerals(num, nanu);
+  return nu.split("").reverse().join("");
+}
